c98be81f6aa7f6eaa5b699389c3676b397720ed4
Muath <muath_hnaifat@yahoo.com>
2019-10-16 00:03:24 +0300

    lab7

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-var Hours = [' 6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var Hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var ShopLocations = [];
 var SalesTable = document.getElementById('TheTable');
 
